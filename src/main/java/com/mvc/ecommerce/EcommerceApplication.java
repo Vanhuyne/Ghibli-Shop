9237989e169e3635692d2e18979b1634f30b39ca
Vanhuyne <106498928+Vanhuyne@users.noreply.github.com>
2024-02-25 17:06:26 +0700

    update init

--- src/main/java/com/mvc/ecommerce/EcommerceApplication.java
+++ src/main/java/com/mvc/ecommerce/EcommerceApplication.java
@@ -2,12 +2,13 @@ package com.mvc.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
 
 }
