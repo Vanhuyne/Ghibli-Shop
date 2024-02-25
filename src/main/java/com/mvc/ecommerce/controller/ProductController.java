c8bc155087603055385d55ab1cb0455934bea2cf
Vanhuyne <106498928+Vanhuyne@users.noreply.github.com>
2024-02-25 17:31:13 +0700

    update init path

--- src/main/java/com/mvc/ecommerce/controller/ProductController.java
+++ src/main/java/com/mvc/ecommerce/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +24,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/")
 public class ProductController {
     private final ProductService productService;
